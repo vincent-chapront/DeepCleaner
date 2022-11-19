7c88698eef147d897910de79a0a15b0c65d43492
vincent-chapront <vincent.chapront@gmail.com>
2022-11-19 19:07:44 +0100

    Fixed manifest

--- DeepCleaner/source.extension.cs
+++ DeepCleaner/source.extension.cs
@@ -7,11 +7,11 @@ namespace DeepCleaner
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "DeepCleaner.c2c9c43c-e52f-4011-b777-0dfb1bd8da88";
-        public const string Name = "DeepCleaner";
+        public const string Id = "VCT.DeepCleaner.c2c9c43c-e52f-4011-b777-0dfb1bd8da88";
+        public const string Name = "VCT.DeepCleaner";
         public const string Description = @"Add command to delete bin and obj with clean and rebuildAdd ";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.0.0";
         public const string Author = "Vincent Chapront";
         public const string Tags = "";
     }
